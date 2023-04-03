7f6b459ea8a6d112b051aee97b166dabaa7e5296
Bryde ツ <6480864+mbryde@users.noreply.github.com>
2023-04-03 10:23:02 +0200

    spring

---
+++ travelguide/src/main/java/com/example/travelguide/TravelguideApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelguideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelguideApplication.class, args);
+    }
+
+}
